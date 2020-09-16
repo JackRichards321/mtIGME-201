012cc59963e0e47ebcc56000de1a232bf81fdd79
Jack Richards <sjackrichards88@gmail.com>
2020-09-16 18:55:35 -0400

    MathQuiz Test1

---
+++ DelegatePE9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePE9
+{
+    class Program
+    {
+
+        delegate string ReadLine;
+
+        static void Main(string[] args)
+        {
+
+            ReadLine processInput;
+
+            processInput = new ReadLine();
+
+        }
+    }
+}
